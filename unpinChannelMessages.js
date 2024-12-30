2c60bf90dfb44913fb0617d09def8655f17787a5
platofff <42435266+platofff@users.noreply.github.com>
2024-12-30 22:10:34 +0300

    unpin channel messages

--- unpinChannelMessages.js
+++ unpinChannelMessages.js
@@ -1,19 +1,20 @@
 export default function setupUnpinChannelMessages (bot) {
   bot.on('message', async (msg) => {
+    // Проверяем, что мы в нужном чате и сообщение автопереслано
     if (
       String(msg.chat.id) === String(process.env.TELEGRAM_CHAT_ID) &&
       msg.is_automatic_forward
     ) {
-      // Если прикреплённое сообщение пришло из нашего канала — сразу открепляем
+      // Проверяем, что отправитель совпадает с каналом, откуда идёт пересылка
       if (
-        String(msg.sender_chat?.id) ===
-        String(process.env.TELEGRAM_CHANNEL_SENDER_CHAT_ID)
+        String(msg.sender_chat?.id) === String(process.env.TELEGRAM_CHANNEL_SENDER_CHAT_ID)
       ) {
         try {
-          await bot.unpinChatMessage(
-            process.env.TELEGRAM_CHAT_ID,
-            msg.message_id
-          )
+          await bot.unpinChatMessage({
+            chat_id: msg.chat.id,
+            message_id: msg.message_id
+          })
+
           console.log('Открепили сообщение из канала', msg.message_id)
         } catch (error) {
           console.error('Не удалось открепить сообщение:', error)
