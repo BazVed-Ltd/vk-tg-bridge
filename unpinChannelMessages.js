3045bc9cfcb3b90b08136137f12786b714adab72
platofff <42435266+platofff@users.noreply.github.com>
2024-12-30 22:29:55 +0300

    unpin channel messages

--- unpinChannelMessages.js
+++ unpinChannelMessages.js
@@ -1,23 +1,19 @@
 export default function setupUnpinChannelMessages (bot) {
   bot.on('message', async (msg) => {
-    // Проверяем, что мы в нужном чате и сообщение автопереслано
-    if (
-      String(msg.chat.id) === String(process.env.TELEGRAM_CHAT_ID) &&
-      msg.is_automatic_forward
-    ) {
-      // Проверяем, что отправитель совпадает с каналом, откуда идёт пересылка
-      if (
-        String(msg.sender_chat?.id) === String(process.env.TELEGRAM_CHANNEL_SENDER_CHAT_ID)
-      ) {
-        try {
-          await bot.unpinChatMessage({
-            chat_id: msg.chat.id,
-            message_id: msg.message_id
-          })
-
-          console.log('Открепили сообщение из канала', msg.message_id)
-        } catch (error) {
-          console.error('Не удалось открепить сообщение:', error)
+    // Проверяем: а точно ли это сообщение - автопересланное из канала?
+    if (msg.is_automatic_forward) {
+      if (String(msg.chat.id) === String(process.env.TELEGRAM_CHAT_ID)) {
+        // Проверяем, что sender_chat совпадает
+        if (String(msg.sender_chat?.id) === String(process.env.TELEGRAM_CHANNEL_SENDER_CHAT_ID)) {
+          try {
+            await bot.unpinChatMessage(
+              msg.message_id,
+              msg.chat.id
+            )
+            console.log('Открепили сообщение из канала', msg.message_id)
+          } catch (error) {
+            console.error('Не удалось открепить сообщение:', error)
+          }
         }
       }
     }
